7a6d45ccbdf46d2152ab65e2dd2d8c3343bda147
kameshsr <47484458+kameshsr@users.noreply.github.com>
2019-10-05 18:09:28 +0530

    Update Assignment1_Basic calculator.java

--- week9/Assignment1_Basic calculator.java
+++ week9/Assignment1_Basic calculator.java
@@ -1,3 +1,19 @@
+/*Complete the code to develop a BASIC CALCULATOR that can perform operations like Addition, Subtraction, Multiplication and Division.
+
+Note the following points carefully:
+1. Use only double datatype to store calculated numeric values.
+2. Assume input to be of integer datatype.
+3. The output should be rounded using Math.round() method.
+4. Take care of the spaces during formatting output (e.g., single space each before and after =).
+5. The calculator should be able to perform required operations on a minimum of two operands as shown in the below example:
+
+Input:
+	       5+6 
+
+Output:
+	       5+6 = 11
+*/
+
 import java.util.Scanner;
 public class Question91{
 	public static void main(String args[]){
@@ -20,3 +36,4 @@ else if (operator=='/')
   System.out.print(input+" = "+Math.round(a/b));
 }
 }
+//https://youtube.com/c/StudyFun for explanation you can see here please subscribe if you like my video
