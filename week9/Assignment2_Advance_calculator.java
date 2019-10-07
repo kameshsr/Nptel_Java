ecc20165ae67382af67ea9b86c9805cd6e54c32d
kameshsr <47484458+kameshsr@users.noreply.github.com>
2019-10-07 10:58:25 +0530

    Update Assignment2_Advance_calculator.java

--- week9/Assignment2_Advance_calculator.java
+++ week9/Assignment2_Advance_calculator.java
@@ -109,10 +109,9 @@ if(ch==1 && check==0){
 
 
 /*
-https://youtube.com/c/StudyFun for explanation you can see here please subscribe if you like my video
- Mail your queries on studyizfun@gmail.com
+Mail your queries on studyizfun@gmail.com
 connect me on linkedin
-https://www.linkedin.com/in/kamesh-sh...
+https://www.linkedin.com/in/kamesh-shekhar-5a3467152/
 If you like my video 
 Please donate at least RS 10 to my paytm wallet 8809692924
 It will help me financially and encourage me to make more videos
