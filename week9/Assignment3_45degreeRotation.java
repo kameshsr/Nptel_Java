a86cbd753cf8bfb8a760b8a926961fb4ae4ea6ca
kameshsr <47484458+kameshsr@users.noreply.github.com>
2019-10-19 15:08:10 +0530

    Update Assignment3_45degreeRotation.java

--- week9/Assignment3_45degreeRotation.java
+++ week9/Assignment3_45degreeRotation.java
@@ -22,68 +22,76 @@ import java.util.Scanner;
 public class Question93{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
-    String arr[]=new String[5];
-    for(int i=0;i<5;i++)
-        arr[i]=sc.nextLine();
-    
-char matrix[][]=new char[5][5];
-for(int i=0;i<5;i++){
-	char[] chararray=arr[i].toCharArray();
-  for(int j=0;j<5;j++){
-  	matrix[i][j]=chararray[j];
-  }
-}
-char newmatrix[][]=new char[5][5];
-for (int i=0;i<5;i++)
-{
-  for(int j=0;j<5;j++)
-  {
-      if(i==0&&j<3)
-          newmatrix[i][j+2]=matrix[i][j];
-      else if(i==0&&j==3)
-          newmatrix[i+1][j+1]=matrix[i][j];
-      else if(i<3&&j==4)
-          newmatrix[i+2][j]=matrix[i][j];
-      else if(i==3&&j==4)
-          newmatrix[i+1][j-1]=matrix[i][j];
-      else if(i==4&&j>1)
-          newmatrix[i][j-2]=matrix[i][j];
-      else if(i==4&&j==1)
-          newmatrix[i-1][j-1]=matrix[i][j];
-      else if(i>1&&j==0)
-          newmatrix[i-2][j]=matrix[i][j];
-      else if(i==1&&j==0)
-          newmatrix[i-1][j+1]=matrix[i][j];
-      else if(i==1&&j>0&&j<4){
-          if(j==3)
-              newmatrix[i+1][j]=matrix[i][j];
-          else
-              newmatrix[i][j+1]=matrix[i][j];
-          
-      }
-      else if(i==2&&j>0&&j<4){
-          if(j==2)
-              newmatrix[i][j]=matrix[i][j];
-          else if(j==3)
-              newmatrix[i+1][j]=matrix[i][j];
-          else
-              newmatrix[i-1][j]=matrix[i][j];
-      }
-      else{
-          if(j==1)
-              newmatrix[i-1][j]=matrix[i][j];
-          else
-              newmatrix[i][j-1]=matrix[i][j];
-      }
-  }
-}
-
-for(int i=0;i<5;i++){
-    for(int j=4;j>=0;j--)
-        System.out.print(newmatrix[i][j]);
-    System.out.println();
-}
-} // The main() method ends here
+			char arr[][]= new char[5][5];
+			// Input 2D Array using Scanner Class
+			for(int line=0;line<5; line++){
+				String input = sc.nextLine();
+				char seq[] = input.toCharArray();
+				if(seq.length==5){
+					for(int i=0;i<5;i++){
+						arr[line][i]=seq[i];
+					}
+				}else{
+					System.out.print("Wrong Input!");
+					System.exit(0);
+				}
+			}
+			// Declaring the array to store Transition
+			char tra[][] = new char[5][5];
+			String outer[]={"00","10","20","30",
+							"40","41","42","43",
+							"44","34","24","14",
+							"04","03","02","01"};
+							
+			String inner[]={"11","21","31","32",
+							"33","23","13","12"};
+			
+			// 45-Degree rotation
+			for(int i=0;i<5;i++){
+				for(int j=0;j<5;j++){
+					// Transform outer portion
+					for(int k=0; k<outer.length; k++){
+						char indices[]=outer[k].toCharArray();
+						int a = Integer.parseInt(String.valueOf(indices[0]));
+						int b = Integer.parseInt(String.valueOf(indices[1]));
+						if(a==i && b==j){
+							if(k==15){k=1;}
+							else if(k==14){k=0;}
+							else {k+=2;}
+							indices=outer[k].toCharArray();
+							a = Integer.parseInt(String.valueOf(indices[0]));
+							b = Integer.parseInt(String.valueOf(indices[1]));
+							tra[a][b] = arr[i][j];
+							break;
+						}
+					}
+					// Transform inner portion
+					for(int k=0; k<inner.length; k++){
+						char indices[]=inner[k].toCharArray();
+						int a = Integer.parseInt(String.valueOf(indices[0]));
+						int b = Integer.parseInt(String.valueOf(indices[1]));
+						if(a==i && b==j){
+							if(k==7){k=0;}
+							else {k+=1;}
+							indices=inner[k].toCharArray();
+							a = Integer.parseInt(String.valueOf(indices[0]));
+							b = Integer.parseInt(String.valueOf(indices[1]));
+							tra[a][b] = arr[i][j];
+							break;
+						}
+					}
+					// Keeping center same
+					tra[2][2] = arr[2][2];
+				}
+			}
+			// Print the transformed output 
+			for(int i=0;i<5;i++){
+				for(int j=0;j<5;j++){
+					System.out.print(tra[i][j]);
+				}
+				System.out.println();
+			}
+	} // The main() method ends here
 } // The main class ends here
 /*
 https://youtube.com/c/StudyFun for explanation you can see here please subscribe if you like my video
